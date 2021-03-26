14ee2c09494760f9d085258b8961deacdb3843c6
Bunyawat Singchai <bunyawat.s@gmail.com>
2021-03-26 11:22:02 +0700

    check point

--- lib/column-alignment/widget/transaction_list.dart
+++ lib/column-alignment/widget/transaction_list.dart
@@ -9,12 +9,16 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions
-          .map((tx) => TransactionCard(
-                transaction: tx,
-              ))
-          .toList(),
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return TransactionCard(
+            transaction: transactions[index],
+          );
+        },
+        itemCount: transactions.length,
+      ),
     );
   }
 }

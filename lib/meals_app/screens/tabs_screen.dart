de458931325ad6a0d6989f9b7821e61111a27e4f
Bunyawat Singchai <bunyawat.s@gmail.com>
2021-04-24 11:00:21 +0700

    checkpoint 174

--- lib/meals_app/screens/tabs_screen.dart
+++ lib/meals_app/screens/tabs_screen.dart
@@ -10,8 +10,58 @@ class TabScreen extends StatefulWidget {
 }
 
 class _TabScreenState extends State<TabScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Your Favorite',
+    },
+  ];
+
+  int _selectPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final Map _selectedPageMap = _pages[_selectPageIndex];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_selectedPageMap['title']),
+      ),
+      body: _selectedPageMap['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectPageIndex,
+        type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       initialIndex: 1,
@@ -24,7 +74,10 @@ class _TabScreenState extends State<TabScreen> {
                 icon: Icon(Icons.category),
                 text: 'Categories',
               ),
-              Tab(icon: Icon(Icons.star), text: 'Favorites')
+              Tab(
+                icon: Icon(Icons.star),
+                text: 'Favorites',
+              )
             ],
           ),
         ),

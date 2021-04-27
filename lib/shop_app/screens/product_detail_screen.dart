b9645e8d2c9da9f84734b444bdd4dc9c303a70a7
Bunyawat Singchai <bunyawat.s@gmail.com>
2021-04-27 14:11:14 +0700

    checkpoint 190

---
+++ lib/shop_app/screens/product_detail_screen.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+
+  final String title;
+
+  const ProductDetailScreen({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+    );
+  }
+}
--- lib/shop_app/widgets/product_item.dart
+++ lib/shop_app/widgets/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
@@ -17,9 +18,20 @@ class ProductItem extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
-        child: Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (ctx) => ProductDetailScreen(
+                  title: title,
+                ),
+              ),
+            );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
         footer: GridTileBar(
           leading: IconButton(

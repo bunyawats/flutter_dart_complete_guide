c41d09a6541b14a82c42a0ba9cbc3f1562a494ca
Bunyawat Singchai <bunyawat.s@gmail.com>
2021-04-29 19:01:13 +0700

    checkpoint 212

--- lib/shop_app/widgets/order_line_item.dart
+++ lib/shop_app/widgets/order_line_item.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'dart:math';
 
 import '../providers/order_provider.dart';
 
-class OrderLineItem extends StatelessWidget {
+class OrderLineItem extends StatefulWidget {
   final OrderItem order;
 
   const OrderLineItem({
     Key key,
     this.order,
   }) : super(key: key);
+
+  @override
+  _OrderLineItemState createState() => _OrderLineItemState();
+}
+
+class _OrderLineItemState extends State<OrderLineItem> {
+  bool _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -20,15 +28,55 @@ class OrderLineItem extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text('\$${order.amount}'),
+            title: Text('\$${widget.order.amount}'),
             subtitle: Text(
-              dateFormat.format(order.dateTime),
+              dateFormat.format(widget.order.dateTime),
             ),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
-          )
+          ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
+              ),
+              height: min(
+                widget.order.productList.length * 20.0 + 10,
+                100,
+              ),
+              child: ListView.builder(
+                itemBuilder: (ctx, index) {
+                  final cartItem = widget.order.productList[index];
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        cartItem.title,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '${cartItem.quantity}x \$${cartItem.price}',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  );
+                },
+                itemCount: widget.order.productList.length,
+              ),
+            )
         ],
       ),
     );

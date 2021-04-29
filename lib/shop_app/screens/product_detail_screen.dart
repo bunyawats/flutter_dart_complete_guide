8450f09057339ad89f494daa72859c3042a76ce2
Bunyawat Singchai <bunyawat.s@gmail.com>
2021-04-29 15:30:19 +0700

    checkpoint 207

--- lib/shop_app/screens/product_detail_screen.dart
+++ lib/shop_app/screens/product_detail_screen.dart
@@ -18,6 +18,44 @@ class ProductDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(loadProduct.title),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$${loadProduct.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              width: double.infinity,
+              child: Text(
+                loadProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }

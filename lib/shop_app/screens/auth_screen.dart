3439858a1cc10a536ad7374ca040b20ce7b1f4af
Bunyawat Singchai <bunyawat.s@gmail.com>
2021-07-28 11:49:58 +0700

    checkpoint 282

--- lib/shop_app/screens/auth_screen.dart
+++ lib/shop_app/screens/auth_screen.dart
@@ -105,7 +105,7 @@ class _AuthCardState extends State<AuthCard>
   final _passwordController = TextEditingController();
 
   AnimationController _controller;
-  Animation<Size> _heightAnimation;
+  // Animation<Size> _heightAnimation;
 
   @override
   void initState() {
@@ -118,15 +118,15 @@ class _AuthCardState extends State<AuthCard>
       ),
     );
 
-    _heightAnimation = Tween<Size>(
-      begin: Size(double.infinity, 260),
-      end: Size(double.infinity, 320),
-    ).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Curves.fastOutSlowIn,
-      ),
-    );
+    // _heightAnimation = Tween<Size>(
+    //   begin: Size(double.infinity, 260),
+    //   end: Size(double.infinity, 320),
+    // ).animate(
+    //   CurvedAnimation(
+    //     parent: _controller,
+    //     curve: Curves.fastOutSlowIn,
+    //   ),
+    // );
 
     // _heightAnimation.addListener(() => setState(() {}));
   }
@@ -225,18 +225,17 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (ctx, ch) => Container(
-          // height: _authMode == AuthMode.Signup ? 320 : 260,
-          height: _heightAnimation.value.height,
-          constraints:
-              // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-              BoxConstraints(minHeight: _heightAnimation.value.height),
-          width: deviceSize.width * 0.75,
-          padding: EdgeInsets.all(16.0),
-          child: ch,
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 600),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        // height: _heightAnimation.value.height,
+        constraints: BoxConstraints(
+          minHeight: _authMode == AuthMode.Signup ? 320 : 260,
         ),
+        // BoxConstraints(minHeight: _heightAnimation.value.height),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
         child: buildForm(context),
       ),
     );

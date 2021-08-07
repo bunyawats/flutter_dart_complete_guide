3a261cf63ce5305e90fcb8059c795b7a1b9a748a
Bunyawat Singchai <bunyawat.s@gmail.com>
2021-08-07 21:59:38 +0700

    checkpoint 304

--- lib/great_place_app/widgets/location_input.dart
+++ lib/great_place_app/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({Key? key}) : super(key: key);
@@ -9,6 +10,12 @@ class LocationInput extends StatefulWidget {
 
 class _LocationInputState extends State<LocationInput> {
   String? _previewImageUrl;
+
+  Future<void> _getCurrentUserLocation() async {
+    final locData = await Location().getLocation();
+    print(locData.latitude);
+    print(locData.longitude);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +49,7 @@ class _LocationInputState extends State<LocationInput> {
               icon: Icon(
                 Icons.location_on,
               ),
-              onPressed: () {},
+              onPressed: _getCurrentUserLocation,
               label: Text('Current Location'),
               style: TextButton.styleFrom(
                 primary: Theme.of(context).primaryColor,

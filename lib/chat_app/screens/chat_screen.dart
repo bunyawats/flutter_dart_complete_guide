b45031380ae757a75131c07acde2c2c5b606bd1b
Bunyawat Singchai <bunyawat.s@gmail.com>
2021-08-12 18:49:55 +0700

    checkpoint 319

--- lib/chat_app/screens/chat_screen.dart
+++ lib/chat_app/screens/chat_screen.dart
@@ -8,30 +8,49 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _stream = FirebaseFirestore.instance
+        .collection('/chats/kNXm2wWvslN6OuRijeYY/messages')
+        .snapshots();
     return Scaffold(
       appBar: AppBar(
         title: Text('Chat Screen'),
       ),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This works'),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('/chats/kNXm2wWvslN6OuRijeYY/messages')
-              .snapshots()
-              .listen((data) {
-            data.docs.forEach((document) {
-              print(document['text']);
-            });
-          });
-        },
-      ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: _stream,
+          builder: (ctx, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(snapshot.error.toString()),
+              );
+            }
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            final docs = snapshot.requireData.docs;
+            return ListView.builder(
+              itemCount: docs.length,
+              itemBuilder: (ctx, index) {
+                final _message = docs[index];
+                return Container(
+                  padding: EdgeInsets.all(8),
+                  child: Text(_message['text']),
+                );
+              },
+            );
+          }),
+      // floatingActionButton: FloatingActionButton(
+      //   child: Icon(Icons.add),
+      //   onPressed: () {
+      //     FirebaseFirestore.instance
+      //         .collection('/chats/kNXm2wWvslN6OuRijeYY/messages')
+      //         .snapshots()
+      //         .listen((data) {
+      //       data.docs.forEach((document) {
+      //         print(document['text']);
+      //       });
+      //     });
+      //   },
+      // ),
     );
   }
 }

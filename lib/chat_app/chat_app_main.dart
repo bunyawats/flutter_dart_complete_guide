eb11aa6bd0cd0eee990e96b266f01d6be4b60a7d
Bunyawat Singchai <bunyawat.s@gmail.com>
2021-08-12 17:52:18 +0700

    checkpoint 317

--- lib/chat_app/chat_app_main.dart
+++ lib/chat_app/chat_app_main.dart
@@ -1,9 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/auth_screen.dart';
 import 'screens/chat_screen.dart';
 
-void main() => runApp(ChatApp());
+/// Requires that a Firestore emulator is running locally.
+/// See https://firebase.flutter.dev/docs/firestore/usage#emulator-usage
+bool USE_FIRESTORE_EMULATOR = false;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  if (USE_FIRESTORE_EMULATOR) {
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  }
+  runApp(ChatApp());
+}
 
 class ChatApp extends StatelessWidget {
   const ChatApp({Key? key}) : super(key: key);
--- lib/chat_app/screens/chat_screen.dart
+++ lib/chat_app/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -17,6 +18,19 @@ class ChatScreen extends StatelessWidget {
           padding: EdgeInsets.all(8),
           child: Text('This works'),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('/chats/kNXm2wWvslN6OuRijeYY/messages')
+              .snapshots()
+              .listen((data) {
+            data.docs.forEach((document) {
+              print(document['text']);
+            });
+          });
+        },
       ),
     );
   }
